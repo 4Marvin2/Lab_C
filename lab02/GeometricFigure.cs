48aa0d75e26814136e9e6d40a98b6837a313df48
Mikhail Popov <mumeu222@mail.ru>
2019-11-10 16:45:26 +0300

    update with lab02

---
+++ lab02/GeometricFigure.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab02
+{
+    abstract class GeometricFigure
+    {
+        public abstract double Square();
+    }
+}
